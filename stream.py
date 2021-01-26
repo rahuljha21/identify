0b9c3405dd92ca1f047e3fe4b439fbe1de86b022
rahuljha21 <77482606+rahuljha21@users.noreply.github.com>
2021-01-26 23:47:55 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,14 +1,14 @@
 import streamlit as st
 import keras as keras
 from PIL import Image
+from keras.models import load_model
 from keras.preprocessing.image import load_img,img_to_array
 from keras.applications.vgg16 import VGG16,preprocess_input,decode_predictions
 def prediction1(img_file):
-    model=VGG16()
-    image=keras.preprocessing.image.load_img(img_file,target_size=(224,224))
+    model=load_model('cotton.h5')
+    image=keras.preprocessing.image.load_img(img_file,target_size=(150,150))
     image=img_to_array(image)
-    image=image.reshape(1,224,224,3)
-    image=preprocess_input(image)
+    image=image.reshape(1,150,150,3)
     label=model.predict(image)
     label=decode_prediction(label)
     label=label[0][0]
