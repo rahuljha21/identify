9cd7ec11378056558614083eda3b72ae04e9f9cc
rahuljha21 <77482606+rahuljha21@users.noreply.github.com>
2021-01-27 10:26:44 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,24 +1,23 @@
 import streamlit as st
 import keras as keras
 from PIL import Image
-from keras.models import load_model
 from keras.preprocessing.image import load_img,img_to_array
 from keras.applications.vgg16 import VGG16,preprocess_input,decode_predictions
+
+
+st.set_option('deprecation.showfileUploaderEncoding',False)
+@st.cache(allow_output_mutation=True)
+model=VGG16()
 uploaded=st.file_uploader('Choose the image')
-
-def prediction1(img_file):
-    model=VGG16()
-    image=load_img(img_file,target_size=(150,150))
-    image=img_to_array(image)
-    image=image.reshape(1,150,150,3)
-    label=model.predict(image)
-    label=decode_prediction(label)
-    label=label[0][0]
-    return label
-
 if uploaded is not None:
     image=Image.open(uploaded)
     st.image(image,caption='Image uploaded',use_column_width=True)
     st.write('Classifying')
-    label=prediction1(uploaded)
+    image=load_img(uploaded,target_size=(224,224))
+    image=img_to_array(image)
+    image=image.reshape((1,224,224,3))
+    image=preprocess_input(image)
+    label=model.predict(image)
+    label=decode_prediction(label)
+    label=label[0][0]
     st.write(label)
